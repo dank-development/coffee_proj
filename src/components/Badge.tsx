5c72c2ce75f6cc98b7a46e12f64fca9bc94d785c
John Doe <none@gmail.com>
2026-04-30 18:29:19 +0200

    changed look and layout

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,14 +1,19 @@
 import { motion } from "motion/react";
+import type { ReactNode } from "react";
 
-export default function Badge() {
+type Props = {
+  children: ReactNode;
+};
+
+export default function Badge({ children }: Props) {
   return (
     <motion.div
       animate={{
         scale: [1, 1.15, 1],
         boxShadow: [
-          "0 0 0px rgba(200,0,0,0.4)",
-          "0 0 8px rgba(200,0,0,1)",
-          "0 0 0px rgba(200,0,0,0.4)",
+          "0 0 0px var(--color-highlight)",
+          "0 0 8px var(--color-highlight)",
+          "0 0 0px var(--color-highlight)",
         ],
       }}
       transition={{
@@ -17,9 +22,9 @@ export default function Badge() {
         repeatType: "loop",
         repeatDelay: 5,
       }}
-      className="bg-highlight w-20 rounded flex justify-center mx-auto"
+      className="flex justify-center bg-highlight text-primary-foreground rounded-full font-semibold text-md absolute right-4 -top-4 px-3 py-1 z-1"
     >
-      <motion.span>Live</motion.span>
+      <span>{children}</span>
     </motion.div>
   );
 }
--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -8,7 +8,7 @@ type Props = {
 export default function CardContainer({ children, className }: Props) {
   return (
     <div
-      className={`w-full grid place-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 bg-surface px-4 py-2 gap-8 ${className ?? ""}`}
+      className={`w-full grid place-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 bg-surface px-4 py-8 gap-8 ${className ?? ""}`}
     >
       {children}
     </div>
